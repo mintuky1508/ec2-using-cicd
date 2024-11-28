cd8f6d04d43ddc3818cd0886aaaf330bdc09905f
mintuky1508 <mintuky1508@gmail.com>
2024-11-28 12:30:21 +0530

    Delete modules directory

--- modules/autoscaling/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "desired_capacity" {}
-variable "max_size" {}
-variable "min_size" {}
-variable "launch_template_id" {}
-variable "subnets" {
-  type = list(string)
-}
-variable "name" {}
-variable "tag_name" {}
